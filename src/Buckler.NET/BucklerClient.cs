c4dd6ae4ab5135d40747e2922b3a10c05710d0b2
gr8stalin <gr8stalin@gmail.com>
2024-10-19 22:37:46 -0400

    Refactor methods to extract repeated SendAsync call into its own method, add more docstrings

--- src/Buckler.NET/BucklerClient.cs
+++ src/Buckler.NET/BucklerClient.cs
@@ -1,4 +1,5 @@
 ﻿using Buckler.NET.Models;
+using System.Net;
 using System.Text.Json;
 
 namespace Buckler.NET
@@ -32,23 +33,15 @@ namespace Buckler.NET
             }
 
             var playerSearchUrl = urlPathGenerator.ProfileSearchUrl(playerName);
-            var request = CreateRequest(playerSearchUrl);
-            var response = await client.SendAsync(request);
+            var playerSearchResponse = await GetBucklerDataAsync(playerSearchUrl);
 
-            if (!response.IsSuccessStatusCode)
-            {
-                return [];
-            }
-
-            var responseString = await response.Content.ReadAsStringAsync();
-            var jsonData = JsonDocument.Parse(responseString).RootElement.GetProperty("pageProps");
             /*
              * The profile list may be empty (i.e., the user doesn't exist), but there's still
              * a parent JSON object wrapping it which contains metadata such as the search terms
              * that were used. This deserialization will always result in an object and will not
              * return null.
              */ 
-            var container = JsonSerializer.Deserialize<PlayerListContainer>(jsonData)!;
+            var container = JsonSerializer.Deserialize<PlayerListContainer>(playerSearchResponse)!;
 
             if (container.PlayerList.Any())
             {
@@ -74,28 +67,26 @@ namespace Buckler.NET
             }
 
             var playerUserCodeSearchUrl = urlPathGenerator.ProfileSearchUrl(playerUserCode);
-            var request = CreateRequest(playerUserCodeSearchUrl);
-            var response = await client.SendAsync(request);
+            var playerSearchResponse = await GetBucklerDataAsync(playerUserCodeSearchUrl);
 
-            if (!response.IsSuccessStatusCode)
-            {
-                return null;
-            }
-
-            var responseString = await response.Content.ReadAsStringAsync();
-            var jsonData = JsonDocument.Parse(responseString).RootElement.GetProperty("pageProps");
             /*
              * The profile list may be empty (i.e., the user doesn't exist), but there's still
              * a parent JSON object wrapping it which contains metadata such as the search terms
              * that were used. This deserialization will always result in an object and will not
              * return null.
              */
-            var container = JsonSerializer.Deserialize<PlayerListContainer>(jsonData)!;
+            var container = JsonSerializer.Deserialize<PlayerListContainer>(playerSearchResponse)!;
             
             return container.PlayerList.SingleOrDefault();
         }
 
-
+        /// <summary>
+        /// Retrieves the most recent replay from the specified user's CFN
+        /// </summary>
+        /// <param name="playerUserCode"></param>
+        /// <param name="gameType"></param>
+        /// <returns>The most recent replay as a <see cref="Replay"/></returns>
+        /// <exception cref="ArgumentException"></exception>
         public async Task<Replay?> GetMostRecentReplayAsync(long? playerUserCode, GameType gameType)
         {
             if (playerUserCode is null)
@@ -104,17 +95,8 @@ namespace Buckler.NET
             }
 
             var replayListUrl = urlPathGenerator.ReplayListUrl(playerUserCode, gameType);
-            var request = CreateRequest(replayListUrl);
-            var response = await client.SendAsync(request);
-
-            if (!response.IsSuccessStatusCode)
-            {
-                return null;
-            }
-
-            var responseString = await response.Content.ReadAsStringAsync();
-            var jsonData = JsonDocument.Parse(responseString).RootElement.GetProperty("pageProps");
-            var replayData = JsonSerializer.Deserialize<ReplayContainer>(jsonData)!;
+            var replayJsonData = await GetBucklerDataAsync(replayListUrl);
+            var replayData = JsonSerializer.Deserialize<ReplayContainer>(replayJsonData)!;
 
             if (!replayData.ReplayList.Any())
             {
@@ -141,57 +123,81 @@ namespace Buckler.NET
             List<Replay> replays = [];
 
             var replayListUrl = urlPathGenerator.ReplayListUrl(playerUserCode, gameType);
-            var request = CreateRequest(replayListUrl);
-            var response = await client.SendAsync(request);
+            var replayResponse = await GetBucklerDataAsync(replayListUrl);
 
-            if (response.IsSuccessStatusCode)
+           /*
+            * The replay list may be empty due to maintenance (i.e., after game updates that break
+            * replay compatibility) but because the underlying JSON will always return a player's
+            * profile data as part of the replay fetching process, the ReplayContainer object will
+            * never be null
+            */ 
+            var replayData = JsonSerializer.Deserialize<ReplayContainer>(replayResponse)!;
+
+            if (!replayData.ReplayList.Any())
             {
-                var responseString = await response.Content.ReadAsStringAsync();
-                var jsonData = JsonDocument.Parse(responseString).RootElement.GetProperty("pageProps");
-                /*
-                 * The replay list may be empty due to maintenance (i.e., after game updates that break
-                 * replay compatibility) but because the underlying JSON will always return a player's
-                 * profile data as part of the replay fetching process, the ReplayContainer object will
-                 * never be null
-                 */ 
-                var replayData = JsonSerializer.Deserialize<ReplayContainer>(jsonData)!;
+                return replays;
+            }
 
-                if (!replayData.ReplayList.Any())
+            replays.AddRange(replayData.ReplayList);
+
+            if (replayData.TotalPages > 1)
+            {
+                for (var i = 2; i <= replayData.TotalPages; i++)
                 {
-                    return replays;
-                }
+                    var nextPageResponse = await GetBucklerDataAsync(replayListUrl + $"?page={i}");
+                    var nextPage = JsonSerializer.Deserialize<ReplayContainer>(nextPageResponse)!;
 
-                replays.AddRange(replayData.ReplayList);
-
-                if (replayData.TotalPages > 1)
-                {
-                    for (var i = 2; i <= replayData.TotalPages; i++)
+                    if (nextPage.ReplayList.Any())
                     {
-                        var nextPageRequest = CreateRequest(replayListUrl + $"?page={i}");
-                        var nextPageResponse = await client.SendAsync(nextPageRequest);
-
-                        if (nextPageResponse.IsSuccessStatusCode)
-                        {
-                            var nextPageAsString = await response.Content.ReadAsStringAsync();
-                            var nextPageJson = JsonDocument.Parse(nextPageAsString).RootElement.GetProperty("pageProps");
-                            var nextPage = JsonSerializer.Deserialize<ReplayContainer>(nextPageJson)!;
-
-                            if (nextPage.ReplayList.Any())
-                            {
-                                replays.AddRange(nextPage.ReplayList);
-                            }
-                            else
-                            {
-                                break;
-                            }
-                        }
+                        replays.AddRange(nextPage.ReplayList);
+                    }
+                    else
+                    {
+                        break;
                     }
                 }
             }
-
+  
             return replays;
         }
 
+        /// <summary>
+        /// Wrapper for <see cref="HttpClient.SendAsync(HttpRequestMessage)"/> with
+        /// some exception handling and Buckler-specific parsing to access the actual
+        /// JSON data within a container property
+        /// </summary>
+        /// <param name="requestUrl"></param>
+        /// <returns></returns>
+        /// <exception cref="WebException"></exception>
+        private async Task<JsonElement> GetBucklerDataAsync(string requestUrl)
+        {
+            var request = CreateRequest(requestUrl);
+            var response = await client.SendAsync(request);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                switch (response.StatusCode)
+                {
+                    case HttpStatusCode.BadRequest:
+                        throw new WebException("The server returned 400: Bad Request");
+                    case HttpStatusCode.Unauthorized:
+                        throw new WebException("The server returned 401: Unauthorized");
+                    case HttpStatusCode.Forbidden:
+                        throw new WebException("The server returned 403: Forbidden");
+                    case HttpStatusCode.NotFound:
+                        throw new WebException("The server returned 404: Not Found");
+                }
+            }
+
+            var responseString = await response.Content.ReadAsStringAsync();
+            return JsonDocument.Parse(responseString).RootElement.GetProperty("pageProps");
+        }
+
+        /// <summary>
+        /// Creates the Buckler-specific <see cref="HttpRequestMessage"/> for
+        /// accessing the various Buckler endpoints
+        /// </summary>
+        /// <param name="searchUrl">The endpoint URL on Buckler</param>
         private HttpRequestMessage CreateRequest(string searchUrl)
         {
             var request = new HttpRequestMessage(HttpMethod.Get, searchUrl);
@@ -203,11 +209,14 @@ namespace Buckler.NET
             return request;
         }
 
+        /// <summary>
+        /// Builds the initial <see cref="HttpClient"/> for this wrapper class
+        /// </summary>
         private static HttpClient CreateHttpClient()
         {
             var handler = new HttpClientHandler()
             {
-                AutomaticDecompression = System.Net.DecompressionMethods.GZip | System.Net.DecompressionMethods.Deflate
+                AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate
             };
 
             return new HttpClient(handler);
